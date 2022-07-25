69c33464edca8c22adcd95e0563753d318d83ba9
schossler <schossler@gmail.com>
2022-07-24 23:49:12 -0300

    Create pix.php

---
+++ pix.php
@@ -0,0 +1,57 @@
+<?php
+
+/* valores a serem configurados */
+$chavepix = ''; //trocar
+$nomebeneficiario = ''; //trocar e não utlizar acentos
+$cidadebeneficiario = ''; //trocar
+$identificador = ''; //trocar
+$valor = ''; //trocar formato 1.12
+
+  function crcChecksum($str) {
+    // The PHP version of the JS str.charCodeAt(i)
+    function charCodeAt($str, $i) {
+      return ord(substr($str, $i, 1));
+    }
+    $crc = 0xFFFF;
+    $strlen = strlen($str);
+    for($c = 0; $c < $strlen; $c++) {
+    $crc ^= charCodeAt($str, $c) << 8;
+      for($i = 0; $i < 8; $i++) {
+        if($crc & 0x8000) {
+          $crc = ($crc << 1) ^ 0x1021;
+        } else {
+          $crc = $crc << 1;
+        }
+     }       
+  }
+  $hex = $crc & 0xFFFF;
+  $hex = dechex($hex);
+  $hex = strtoupper($hex);
+        
+  return str_pad($hex, 4, '0', STR_PAD_LEFT);
+}
+
+$pix = '00020126'.(strlen($chavepix)+22).'0014BR.GOV.BCB.PIX01'.str_pad(strlen($chavepix), 2, '0', STR_PAD_LEFT).$chavepix.
+       '52040000530398654'.str_pad(strlen($valor), 2, '0', STR_PAD_LEFT).$valor.'5802BR59'.str_pad(strlen($nomebeneficiario), 2, '0', STR_PAD_LEFT).$nomebeneficiario.
+       '60'.str_pad(strlen($cidadebeneficiario), 2, '0', STR_PAD_LEFT).$cidadebeneficiario.'621305'.str_pad(strlen($identificador), 2, '0', STR_PAD_LEFT).$identificador.'6304';
+$pix = $pix.crcChecksum($pix);
+$qrcode = 'https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl='.rawurlencode($pix);
+?>
+
+<p>
+<script type="text/javascript">function copy() {  let textarea = document.getElementById("pixcc");  textarea.select();  document.execCommand("copy");}</script>
+Para proceder o pagamento seguir as instruções abaixo:
+<br /><br />
+PIX CHAVE: <br />
+<?= $chavepix ?>
+<br /><br />
+PIX COPY COLA:<br />
+<textarea id='pixcc' readonly><?= $pix ?></textarea><br />
+<button onclick="copy()">Copiar</button>
+<br /><br />
+PIX QR-CODE:<br />
+<img src=<?= $qrcode ?> />
+<br />
+O prazo para PIX é de 3 dias úteis, após esse prazo o pedido será cancelado automaticamente.
+<br />
+</p>
